b428d5acf9d2c632ec7af27a2e3ed9a7cd060afe
SuniRein <sunirein@qq.com>
2025-08-07 23:20:44 +0800

    chore(deps): add vue-i18n

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,17 @@
+import vueI18n from '@intlify/unplugin-vue-i18n/vite';
 import { defineConfig } from 'wxt';
 
 export default defineConfig({
     srcDir: 'src',
     imports: false,
     modules: ['@wxt-dev/module-vue'],
+    vite: () => ({
+        plugins: [
+            vueI18n({
+                include: 'assets/locales/*.json',
+            }),
+        ],
+    }),
     manifest: {
         name: 'Read It Later Simply',
         permissions: ['tabs', 'storage', 'downloads'],
