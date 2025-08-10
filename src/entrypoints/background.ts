7102cc6727548a7f2063028947826ad189d3ccdf
SuniRein <sunirein@qq.com>
2025-08-10 21:28:27 +0800

    feat(badge): change badge color when current tab is in the page list, closed #20

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,12 +1,18 @@
 import type { Browser } from '#imports';
+
 import { browser, defineBackground } from '#imports';
 
+import { computed, shallowRef, watch } from 'vue';
 import { sendMessage } from '@/utils/message';
 import store from '@/utils/store';
 
 const action = browser.action ?? browser.browserAction;
 
+const commonBadgeColor = '#444';
+const activeBadgeColor = '#16a34a';
+
 export default defineBackground(() => {
+    // connection with popup
     let popupPort: Browser.runtime.Port | null = null;
 
     browser.runtime.onConnect.addListener((port) => {
@@ -18,36 +24,52 @@ export default defineBackground(() => {
         }
     });
 
-    async function broadcastCurrentTab(tab: Browser.tabs.Tab) {
-        if (!popupPort)
-            return;
-        await sendMessage('currentTabChanged', { tab });
+    // update current tab
+    const currentTabUrl = shallowRef<string>('');
+
+    async function updateCurrentTab(tab: Browser.tabs.Tab) {
+        currentTabUrl.value = tab.url ?? '';
+        if (popupPort)
+            await sendMessage('currentTabChanged', { tab });
     }
 
     browser.tabs.onActivated.addListener(async (activeInfo) => {
         const tab = await browser.tabs.get(activeInfo.tabId);
-        await broadcastCurrentTab(tab);
+        await updateCurrentTab(tab);
     });
 
     browser.tabs.onUpdated.addListener(async (_tabId, _changeInfo, tab) => {
         if (tab.active) {
-            await broadcastCurrentTab(tab);
+            await updateCurrentTab(tab);
         }
     });
 
-    action.setBadgeBackgroundColor({ color: '#444' });
-    updateBadgeText();
+    // show badge with page count
+    const showBadge = shallowRef(false);
+    store.setting.getValue().then(setting => showBadge.value = setting.showPageCount);
+    store.setting.watch(setting => showBadge.value = setting.showPageCount);
 
-    store.setting.watch(updateBadgeText);
-    store.pageList.watch(updateBadgeText);
-});
+    const pageUrls = shallowRef<string[]>([]);
+    store.pageList.getValue().then(pageList => pageUrls.value = pageList.map(item => item.info.url));
+    store.pageList.watch(pageList => pageUrls.value = pageList.map(item => item.info.url));
 
-async function updateBadgeText() {
-    if ((await store.setting.getValue()).showPageCount) {
-        const count = (await store.pageList.getValue()).length;
+    const currentTabActive = computed(() => pageUrls.value.includes(currentTabUrl.value));
+
+    function updateBadge() {
+        if (!showBadge.value) {
+            action.setBadgeText({ text: '' });
+            return;
+        }
+
+        const currentTabActive = pageUrls.value.includes(currentTabUrl.value);
+        action.setBadgeBackgroundColor({
+            color: currentTabActive ? activeBadgeColor : commonBadgeColor,
+        });
+
+        const count = pageUrls.value.length;
         action.setBadgeText({ text: count.toString() });
     }
-    else {
-        action.setBadgeText({ text: '' });
-    }
-}
+    updateBadge();
+
+    watch([showBadge, pageUrls, currentTabActive], updateBadge);
+});
