d152aa089ba0906947db04e0044363f924da31e5
SuniRein <sunirein@qq.com>
2025-10-19 22:30:53 +0800

    feat(search): support negation filter, closed #54

--- src/composables/page-list-filtered.ts
+++ src/composables/page-list-filtered.ts
@@ -3,26 +3,52 @@ import type { FavoritedFilterOption, PageItem } from '@/utils/types';
 
 import { computed, unref } from 'vue';
 
-function searchFilter(searchText: string) {
-    return (item: PageItem): boolean => {
-        if (searchText.length === 0)
-            return true;
-        return searchText.split(/\s+/).every((token) => {
-            if (token.length === 0)
-                return true;
+type Filter = (item: PageItem) => boolean;
 
-            if (token.startsWith('#')) {
-                const tag = token.slice(1);
-                return item.tags.includes(tag);
-            }
+function searchFilter(searchText: string): Filter {
+    const tokens = searchText.split(/\s+/);
 
-            const kw = token.toLowerCase();
-            return item.info.title.toLowerCase().includes(kw) || item.info.url.toLowerCase().includes(kw);
-        });
-    };
+    function parseToken(token: string): Filter | null {
+        if (token.startsWith('!')) {
+            const innerToken = token.slice(1);
+            const f = parseTokenWithoutNegation(innerToken);
+            return f ? negateFilter(f) : null;
+        }
+        return parseTokenWithoutNegation(token);
+    }
+
+    function parseTokenWithoutNegation(token: string): Filter | null {
+        if (token.length === 0)
+            return null;
+
+        if (token.startsWith('#')) {
+            const tag = token.slice(1);
+            return byTag(tag);
+        }
+
+        const kw = token.toLowerCase();
+        return byKeyword(kw);
+    }
+
+    function negateFilter(f: Filter): Filter {
+        return item => !f(item);
+    }
+
+    function byTag(tag: string): Filter | null {
+        if (tag.length === 0)
+            return null;
+        return item => item.tags.includes(tag);
+    }
+
+    function byKeyword(kw: string): Filter {
+        return item => item.info.title.toLowerCase().includes(kw) || item.info.url.toLowerCase().includes(kw);
+    }
+
+    const filters = tokens.map(parseToken).filter(fn => fn !== null);
+    return item => filters.every(fn => fn(item));
 }
 
-function favoritedFilter(favoritedFilterOption: FavoritedFilterOption) {
+function favoritedFilter(favoritedFilterOption: FavoritedFilterOption): Filter {
     return (item: PageItem): boolean => {
         if (favoritedFilterOption === 'all')
             return true;
